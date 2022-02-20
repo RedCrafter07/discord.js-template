b1ee75604429befcbc8820e6a54d27bed75d2df8
RedCrafter07 <github@redcrafter07.de>
2022-02-20 12:54:27 +0100

    Default config

---
+++ config.ts
@@ -0,0 +1,3 @@
+export const TOKEN = 'TOKEN'; //replace with your token
+export const GLOBAL = false; //if true, commands will be registered to all guilds.
+export const MAIN_GUILD = 'GUILD_ID'; //only required if GLOBAL is false.
